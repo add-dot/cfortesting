35c9451a2123841ffa17e19d18c2fd469215fbd4
var0g <alvaro.droppelmann.d@gmail.com>
2025-02-17 00:05:51 -0300

    Add: New logic to reverse the version of c.. listed on the api fetch

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 use dirs::home_dir;
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Client;
 use serde::Deserialize;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::path::absolute;
-use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::Client;
-use futures_util::StreamExt;
 
 const URL: &str =
     "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json";
+
+const ENVIRO_DIR: &str = "/.cft/";
 
 #[derive(Debug, Deserialize)]
 struct ApiResponse {
     versions: Vec<Version>,
 }
 
+//TODO: create specifics errors
 #[derive(Debug, PartialEq)]
 enum CreationDirError {
     BadPath(String),
@@ -55,6 +58,8 @@ async fn fetch_latest_chrome_download_url(
     platform: &str,
     browser_version: &str,
 ) -> Result<String, Box<dyn Error>> {
+    //TODO: create new function to fetch specifics versions
+    //TODO: create cache informationg  base on time and disk?
     use reqwest::header;
     let mut headers = header::HeaderMap::new();
     headers.insert(
@@ -69,7 +74,7 @@ async fn fetch_latest_chrome_download_url(
 
     let mut downloable_version: String = String::default();
 
-    for version in response.versions {
+    for version in response.versions.iter().rev() {
         if let Some(download) = version
             .downloads
             .chrome
@@ -115,9 +120,11 @@ async fn download_browser(
 
     // Barra de progreso
     let pb = ProgressBar::new(content_length);
-    pb.set_style(ProgressStyle::default_bar()
-        .template("[{elapsed_precise}] {bar:40.cyan/blue} {bytes}/{total_bytes} ({eta})")?
-        .progress_chars("#>-"));
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40.cyan/blue} {bytes}/{total_bytes} ({eta})")?
+            .progress_chars("#>-"),
+    );
 
     if response.status().is_success() {
         let mut f = File::create(target_file_os + "downloaded_file.zip")?;
@@ -140,23 +147,24 @@ async fn download_browser(
 fn create_dir() -> Result<String, Box<dyn std::error::Error>> {
     let home = home_dir().unwrap();
     let home_parsed = home.to_str().unwrap();
-    let absolute_home = absolute(home_parsed.to_owned() + "/.cft/").unwrap();
+    let absolute_home = absolute(home_parsed.to_owned() + ENVIRO_DIR).unwrap();
     let parsed_absolute = absolute_home.to_str().unwrap();
     let dir_all = fs::create_dir_all(parsed_absolute);
     match dir_all {
         Ok(..) => Ok(parsed_absolute.to_string()),
-        Err(e) => panic!("{e:?}"),
+        Err(e) => panic!("Error when creating directories for cft: {e:?}"),
     }
 }
+
 #[tokio::main]
 async fn main() {
     let platform = get_platform();
     let parsed_absolute = create_dir().unwrap();
-    println!("{parsed_absolute:?}");
     let version: &str = "latest";
     let cft_api_response = fetch_latest_chrome_download_url(platform, version)
         .await
         .unwrap();
-    println!("{cft_api_response:?}");
-    let _ = download_browser(cft_api_response, parsed_absolute).await;
+    let _ = download_browser(cft_api_response, parsed_absolute)
+        .await
+        .ok();
 }
