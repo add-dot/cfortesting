c262278960eda866be09179f72dcf109e3e8ca33
var0g <alvaro.droppelmann.d@gmail.com>
2026-09-04 17:14:42 -0400

    Rename folder to fit more with the objective of the module

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ mod downloadable;
 mod extraction;
 mod parse_install;
 mod tests;
-mod unixs;
+mod sys;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     const URL_GV: &str =
         "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json";
     const URL_LTS_GK: &str = "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json";
-    let platform = unixs::utils::get_platform();
-    let parsed_absolute = unixs::utils::create_dir().unwrap_or_else(|err| {
+    let platform = sys::utils::get_platform();
+    let parsed_absolute = sys::utils::create_dir().unwrap_or_else(|err| {
         eprintln!("Error when initilizing enviroment {err:?}");
         std::process::exit(1);
     });
@@ -50,7 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             };
 
-            let extraction_path_str = unixs::utils::tree_directory(&parsed_absolute, type_downloable, &version_string);
+            let extraction_path_str = sys::utils::tree_directory(&parsed_absolute, type_downloable, &version_string);
             let target_file_os = downloadable::download_browser(
                 url,
                 version_string,
