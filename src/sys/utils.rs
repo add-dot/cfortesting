8c3b9d735f2ae085c0cff62f6f315ab0dd84d31c
var0g <alvaro.droppelmann.d@gmail.com>
2026-09-05 20:56:46 -0400

    Changed the way how we use the paths on this changes we use only path/pathbuf to handle the diferent paths that way we reduce de conversions and we have standar for paths

--- src/sys/utils.rs
+++ src/sys/utils.rs
@@ -1,24 +1,20 @@
 use dirs::home_dir;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Error {
     CannotCreateDir,
     HomeDirNotFound,
-    InvalidPathUtf8,
 }
 
-pub fn create_dir() -> Result<String, Error> {
+pub fn create_dir() -> Result<PathBuf, Error> {
     let home: std::path::PathBuf = home_dir().ok_or(Error::HomeDirNotFound)?;
     let target_dir = home.join(".cft");
     if fs::create_dir_all(&target_dir).is_err() {
         return Err(Error::CannotCreateDir);
     }
-    target_dir
-        .to_str()
-        .map(std::string::ToString::to_string)
-        .ok_or(Error::InvalidPathUtf8)
+    Ok(target_dir)
 }
 
 pub fn get_platform() -> &'static str {
@@ -33,12 +29,9 @@ pub fn get_platform() -> &'static str {
 }
 
 pub fn tree_directory(
-    parsed_absoluted: &str,
+    parsed_absoluted: &Path,
     type_downloable: &str,
     version_string: &str,
-) -> String {
-    let final_extraction_path = Path::new(parsed_absoluted)
-        .join(type_downloable)
-        .join(version_string);
-    final_extraction_path.to_string_lossy().to_string()
+) -> PathBuf {
+    parsed_absoluted.join(type_downloable).join(version_string)
 }
